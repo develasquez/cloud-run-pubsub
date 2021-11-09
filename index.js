a6c9ea572725ce3ecce7655eb10711813ff38fa7
Felipe Velásquez <felivelasquez@google.com>
2021-11-09 14:31:56 -0300

    File

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
 const Buffer = require('safe-buffer').Buffer;
+const Storage = require('@google-cloud/storage');
+const storage = Storage();
 const express = require('express');
 const app = express();
 app.use(express.json());
 
 app.post('/', (req, res) => {
     if (!req.body) {
-      const msg = 'no Pub/Sub message received';
-      console.error(`error: ${msg}`);
-      res.status(400).send(`Bad Request: ${msg}`);
-      return;
+        const msg = 'no Pub/Sub message received';
+        console.error(`error: ${msg}`);
+        res.status(400).send(`Bad Request: ${msg}`);
+        return;
     }
     if (!req.body.message) {
-      const msg = 'invalid Pub/Sub message format';
-      console.error(`error: ${msg}`);
-      res.status(400).send(`Bad Request: ${msg}`);
-      return;
-    }  
-  
+        const msg = 'invalid Pub/Sub message format';
+        console.error(`error: ${msg}`);
+        res.status(400).send(`Bad Request: ${msg}`);
+        return;
+    }
+
     const pubSubMessage = req.body.message;
-    const data = pubSubMessage.data
-      ? Buffer.from(pubSubMessage.data, 'base64').toString().trim()
-      : ''; 
+    const data = JSON.parse(pubSubMessage.data
+        ? Buffer.from(pubSubMessage.data, 'base64').toString().trim()
+        : '{}');
     console.log(data);
+
+    readFile(data.bucket, data.name);
     res.status(204).send();
-  });
+});
 
-  const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-  app.listen(PORT, () =>
+app.listen(PORT, () =>
     console.log(`nodejs-pubsub-tutorial listening on port ${PORT}`)
-  );
-  
+);
+
+
+readFile = (bucket, name) => {
+    console.log('Reading File');
+    var archivo = storage.bucket(bucket).file(name).createReadStream();
+    console.log('Concat Data');
+    var buf = '';
+    archivo.on('data', function (d) {
+        buf += d;
+    }).on('end', function () {
+        console.log(buf);
+        console.log("End");
+    });
+};
 
 /*
 //const { PubSub } = require(`@google-cloud/pubsub`);
